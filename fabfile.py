29ca887c796cc4a74543786ea862dc055d3b2efd
Bernard Kolobara <bernard@cliqz.com>
2014-06-16 11:58:54 +0200

    Set default package to beta and ask before publish

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,7 @@
 import urllib2
 import xml.etree.ElementTree as ET
 
+from fabric.contrib import console
 from fabric.api import task, local, lcd, hide
 from fabric.utils import abort
 from jinja2 import Environment, FileSystemLoader
@@ -17,7 +18,7 @@ XML_EM_NAMESPACE = "http://www.mozilla.org/2004/em-rdf#"
 AUTO_INSTALLER_URL = "http://localhost:8888/"
 
 
-def get_version(beta=False):
+def get_version(beta='True'):
     """Returns the extension's version string.
 
     The returned version will be constructed from the biggest version tag. If
@@ -28,7 +29,7 @@ def get_version(beta=False):
     full_version = local("git describe --tags", capture=True)  # e.g. 0.4.08-2-gb4f9f56
     version_parts = full_version.split("-")
     version = version_parts[0]
-    if beta:
+    if beta == 'True':
         # If the number of commits after a tag is 0 the returned versions have
         # no dashes (e.g. 0.4.08)
         try:
@@ -39,13 +40,16 @@ def get_version(beta=False):
 
 
 @task
-def package(beta=False):
+def package(beta='True'):
     """Package the extension as a .xpi file."""
     version = get_version(beta)
 
     # If we are not doing a beta release we need to checkout the latest stable tag
-    if not beta:
+    if not (beta == 'True'):
         with hide('output'):
+            # Because the file install.rdf changes after generating from template
+            # we need to untrack it before we can do a stash-pop.
+            local("git update-index --assume-unchanged cliqz@cliqz.com/install.rdf")
             local("git stash")
             local("git checkout %s" % (version))
 
@@ -68,7 +72,7 @@ def package(beta=False):
             local("mv  %s .." % output_file_name)  # Move back to root folder
 
     # If we checked out a earlier commit we need to go back to master/HEAD
-    if not beta:
+    if not (beta == 'True'):
         with hide('output'):
             local("git checkout -f master")
             local("git stash pop")
@@ -77,7 +81,7 @@ def package(beta=False):
 
 
 @task
-def install_in_browser(beta=False):
+def install_in_browser(beta='True'):
     """Install the extension in firefox.
 
     Firefox needs the Extension Auto-Installer add-on.
@@ -95,18 +99,28 @@ def install_in_browser(beta=False):
 
 
 @task
-def publish(beta=False):
+def publish(beta='True'):
     """Upload extension to s3 (credentials in ~/.s3cfg need to be set to primary)"""
+
+    if beta == 'True':
+        if not console.confirm('You are going to update the extension '\
+                               'for BETA users. Do you want to continue?'):
+            return
+    else:
+        if not console.confirm('You are going to update the extension '\
+                               'for ALL users. Do you want to continue?'):
+            return
+
     update_manifest_file_name = "latest.rdf"
     latest_html_file_name = "latest.html"
     output_file_name = package(beta)
-    path_to_s3 = PATH_TO_S3_BETA_BUCKET if beta else PATH_TO_S3_BUCKET
+    path_to_s3 = PATH_TO_S3_BETA_BUCKET if beta == 'True' else PATH_TO_S3_BUCKET
     local("s3cmd --acl-public put %s %s" % (output_file_name, path_to_s3))
 
     env = Environment(loader=FileSystemLoader('templates'))
     manifest_template = env.get_template(update_manifest_file_name)
     version = get_version()
-    if beta:
+    if beta == 'True':
         download_link = "https://s3.amazonaws.com/cdncliqz/update/beta/%s" % output_file_name
     else:
         download_link = "https://s3.amazonaws.com/cdncliqz/update/%s" % output_file_name
@@ -119,7 +133,7 @@ def publish(beta=False):
     local("rm  %s" % update_manifest_file_name)
 
     # Provide a link to the latest stable version
-    if not beta:
+    if not (beta == 'True'):
         latest_template = env.get_template(latest_html_file_name)
         output_from_parsed_template = latest_template.render(download_link=download_link)
         with open(latest_html_file_name, "wb") as f:
