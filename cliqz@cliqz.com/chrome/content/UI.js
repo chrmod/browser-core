d015ec4db340d92cc3e74942d677be23e166870c
Dmitri <dmitri.m@cliqz.com>
2014-10-02 11:28:42 +0200

    bitcoin special

--- cliqz@cliqz.com/chrome/content/UI.js
+++ cliqz@cliqz.com/chrome/content/UI.js
@@ -2,7 +2,7 @@
 
 (function(ctx) {
 
-var TEMPLATES = ['main', 'results', 'suggestions', 'emphasis', 'empty', 'text', 'generic', 'custom', 'clustering', 'series', 'oktoberfest'],
+var TEMPLATES = ['main', 'results', 'suggestions', 'emphasis', 'empty', 'text', 'generic', 'custom', 'clustering', 'series', 'oktoberfest', 'bitcoin'],
     VERTICALS = {
         'b': 'bundesliga',
         'w': 'weather' ,
