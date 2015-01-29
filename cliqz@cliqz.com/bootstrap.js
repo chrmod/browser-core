705d3d91e66f440a474697d9ab7f97ddf359329c
Sean Gustafson <sean@cliqz.com>
2015-01-29 12:30:45 +0100

    Remove unused AB tests.

--- cliqz@cliqz.com/bootstrap.js
+++ cliqz@cliqz.com/bootstrap.js
@@ -7,9 +7,9 @@ XPCOMUtils.defineLazyModuleGetter(this, 'Extension',
 
 function startup(aData, aReason) {
     Extension.load(aReason == ADDON_UPGRADE);
-    //if (aReason == ADDON_ENABLE || aReason == ADDON_INSTALL)
-    //    CliqzUtils.setOurOwnPrefs();
-    //    TODO: to ABTest, for now
+    if (aReason == ADDON_ENABLE || aReason == ADDON_INSTALL)
+        CliqzUtils.setOurOwnPrefs();
+
 }
 
 function shutdown(aData, aReason) {
