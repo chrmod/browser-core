6fefd9c84a294856f05c1e8e8e81716b789133b0
Sven Unnewehr <sven@cliqz.com>
2015-02-19 14:48:54 +0100

    Added keyboard scrolling

--- cliqz@cliqz.com/chrome/content/UI.js
+++ cliqz@cliqz.com/chrome/content/UI.js
@@ -977,6 +977,81 @@ function clearResultSelection(keepArrow){
     if(title)title.style.textDecoration = "none";
 }
 
+/**
+    Smoothly scroll element to the given target (element.scrollTop)
+    for the given duration
+
+    Returns a promise that's fulfilled when done, or rejected if
+    interrupted
+ */
+var smooth_scroll_to = function(element, target, duration) {
+    target = Math.round(target);
+    duration = Math.round(duration);
+    if (duration < 0) {
+        return Promise.reject("bad duration");
+    }
+    if (duration === 0) {
+        element.scrollTop = target;
+        return Promise.resolve();
+    }
+
+    var start_time = Date.now();
+    var end_time = start_time + duration;
+
+    var start_top = element.scrollTop;
+    var distance = target - start_top;
+
+    // based on http://en.wikipedia.org/wiki/Smoothstep
+    var smooth_step = function(start, end, point) {
+        if(point <= start) { return 0; }
+        if(point >= end) { return 1; }
+        var x = (point - start) / (end - start); // interpolation
+        return x*x*(3 - 2*x);
+    }
+
+    return new Promise(function(resolve, reject) {
+        // This is to keep track of where the element's scrollTop is
+        // supposed to be, based on what we're doing
+        var previous_top = element.scrollTop;
+
+        // This is like a think function from a game loop
+        var scroll_frame = function() {
+            if(element.scrollTop != previous_top) {
+                reject("interrupted");
+                return;
+            }
+
+            // set the scrollTop for this frame
+            var now = Date.now();
+            var point = smooth_step(start_time, end_time, now);
+            var frameTop = Math.round(start_top + (distance * point));
+            element.scrollTop = frameTop;
+
+            // check if we're done!
+            if(now >= end_time) {
+                resolve();
+                return;
+            }
+
+            // If we were supposed to scroll but didn't, then we
+            // probably hit the limit, so consider it done; not
+            // interrupted.
+            if(element.scrollTop === previous_top
+                && element.scrollTop !== frameTop) {
+                resolve();
+                return;
+            }
+            previous_top = element.scrollTop;
+
+            // schedule next frame for execution
+            setTimeout(scroll_frame, 0);
+        }
+
+        // boostrap the animation process
+        setTimeout(scroll_frame, 0);
+    });
+}
+
 function setResultSelection(el, scroll, scrollTop, changeUrl, mouseOver){
     if(el){
         //focus on the title - or on the aroww element inside the element
@@ -991,8 +1066,13 @@ function setResultSelection(el, scroll, scrollTop, changeUrl, mouseOver){
             //arrow target is now on an inner element
             el.removeAttribute('arrow');
 
+        var offset = target.offsetTop;
+        if(target.className.indexOf("cliqz-pattern") != -1) offset += $('.cqz-result-pattern', gCliqzBox).parentNode.offsetTop;
+        var scroll = parseInt(offset/303) * 303;
+        if(!mouseOver) smooth_scroll_to(gCliqzBox.resultsBox, scroll, 800);
+
         target.setAttribute('arrow', 'true');
-        arrow.style.top = (target.offsetTop + target.offsetHeight/2 - 7) + 'px';
+        arrow.style.top = (offset + target.offsetHeight/2 - 7) + 'px';
         arrow.setAttribute('active', 'true');
         var title = $('.cqz-ez-title', el) || $('.cqz-result-title', el) || $('.cliqz-pattern-element-title', el);
         if(title) title.style.textDecoration = 'underline';
