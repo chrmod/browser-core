88454262e7c22b9d9412b4ee0ebb2f9c3d538a1b
Lucian Corlaciu <lucian@cliqz.com>
2015-03-09 11:33:32 +0100

    fixes https://github.com/cliqz/bug-tracker/issues/395

--- cliqz@cliqz.com/chrome/content/UI.js
+++ cliqz@cliqz.com/chrome/content/UI.js
@@ -127,7 +127,7 @@ var UI = {
         isInstant: lastRes && lastRes.isInstant
       });
 
-      if(currentResults.results && currentResults.results.length > 0)
+      if(currentResults.results && currentResults.results.length > 0 && currentResults.results[0].url)
         CLIQZ.Core.autocompleteQuery(CliqzUtils.cleanMozillaActions(currentResults.results[0].url), currentResults.results[0].title);
     },
     results: function(res){
