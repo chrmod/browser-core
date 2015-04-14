af56aa09a68029675184908ab77d46e2f09f8dfc
Sven Unnewehr <sven@cliqz.com>
2015-04-14 14:21:20 +0200

    Cleanup

--- cliqz@cliqz.com/chrome/content/core.js
+++ cliqz@cliqz.com/chrome/content/core.js
@@ -522,7 +522,6 @@ window.CLIQZ.Core = {
 
         // Detect autocomplete
         var autocomplete = CliqzHistoryPattern.autocompleteTerm(urlBar.value, results[0], true);
-        CliqzUtils.log(results);
         if(!autocomplete.autocomplete && fRes.length > 1 && CliqzHistoryPattern.generalizeUrl(fRes[0].url) != urlBar.value) {
           autocomplete = CliqzHistoryPattern.autocompleteTerm(urlBar.value, fRes[1], true);
           CLIQZ.UI.autocompleteEl = 1;
