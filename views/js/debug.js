e801bf7c17aafd260d79a61377a57b9167f6dd71
Lucian Corlaciu <lucian@cliqz.com>
2015-04-27 15:52:22 +0200

    clean debug script

--- views/js/debug.js
+++ views/js/debug.js
@@ -5,29 +5,18 @@ urlbar.onkeyup = function(e){
 	CLIQZ.UI.main(document.getElementById('results'));
 	CliqzUtils.getCliqzResults(urlbar.value, function(r){
 		var res = JSON.parse(r.response)
-		console.log('return', res);
 
-
-      /*for(var i=0; i<popup._matchCount; i++) {
-          data.push({
-            title: ctrl.getCommentAt(i),
-            url: unEscapeUrl(ctrl.getValueAt(i)),
-            type: ctrl.getStyleAt(i),
-            text: q,
-            data: lastRes && lastRes.getDataAt(i),
-          });
-      }*/
-
-      var currentResults = CLIQZ.UI.results({
-        q: res.q,
-        results: res.result.map(function(r){
-        	r.type='';
-        	r.title = r.title || '';
-        	r.text = res.q;
-        	r.value = r.url;
-        	return r;
-        }),
-        isInstant: false
-      });
+		var currentResults = CLIQZ.UI.results({
+			q: res.q,
+			results: res.result.map(function(r){
+				//Lucian: we need some more modules in the middle
+				r.type='';
+				r.title = r.title || '';
+				r.text = res.q;
+				r.value = r.url;
+				return r;
+			}),
+			isInstant: false
+		});
 	})
 }
