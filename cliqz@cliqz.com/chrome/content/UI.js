4c3089562f650e1fdb948042a12e57983898aa23
Dominik Schmidt <dominik.s@cliqz.com>
2015-05-13 18:05:51 +0200

    hotfoot for issue 622

--- cliqz@cliqz.com/chrome/content/UI.js
+++ cliqz@cliqz.com/chrome/content/UI.js
@@ -925,6 +925,7 @@ function enhanceResults(res){
         "footer-message": {
           message: CliqzUtils.getLocalizedString('updateMessage'),
           telemetry: 'changelog',
+          searchTerm: '',
           options: [{
               text: CliqzUtils.getLocalizedString('updatePage'),
               action: 'update-show',
@@ -983,6 +984,7 @@ function getNotSupported(){
     message: CliqzUtils.getLocalizedString('OutOfCoverageWarning'),
     telemetry: 'international',
     type: 'cqz-message-alert',
+    searchTerm: '',
     options: [{
         text: CliqzUtils.getLocalizedString('keep-cliqz'),
         action: 'keep-cliqz',
