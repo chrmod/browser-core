c086edbd823172baa7c82e040b57194662d38ac9
giota-cliqz <panagiota@cliqz.com>
2015-07-22 12:25:32 +0200

    fix local script

--- cliqz@cliqz.com/tests/scripts/runTestsLocal.sh
+++ cliqz@cliqz.com/tests/scripts/runTestsLocal.sh
@@ -4,4 +4,4 @@ clear
 
 echo "Opening firefox"
 
-open -a Firefox --args  -chrome chrome://cliqz/content/tests/scripts/run.html -p dev --no-remote
+open -a Firefox --args -chrome chrome://cliqztests/content/scripts/run.html -p dev --no-remote
