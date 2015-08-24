465e05b37422939db4232c43ad2c607f9b44276c
Lucian Corlaciu <lucian@cliqz.com>
2015-08-24 14:20:22 +0200

    telemetry test -> environment

---
+++ specific/firefox/cliqz@cliqz.com/tests/integration/TelemetryTest.js
@@ -0,0 +1,22 @@
+TESTS.TelemetryTest = function (CliqzUtils) {
+  describe('Telemetry', function(){
+   it('should send environment signal', function () {
+      var signal = null;
+      CliqzUtils.telemetry = function(data){
+        signal = data;
+      }
+      CliqzUtils.getWindow().CLIQZ.Core.whoAmI(true);
+
+      return waitFor(function () {
+        return signal != null;
+      }).then(function(){
+        chai.expect(signal.type).to.equal('environment');
+        chai.expect(signal.agent).to.contain('Mozilla');
+        chai.expect(signal.version).to.exist;
+        chai.expect(signal.prefs.session).to.exist;
+        chai.expect(signal.prefs.config_location).to.exist;
+        chai.expect(signal.prefs.ABTests).to.exist;
+      });
+    });
+  });
+};
