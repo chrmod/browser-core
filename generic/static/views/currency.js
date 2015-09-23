1ce642678fc2408bf83644a1cce749a84c170ba8
Gabriel <gabriel@cliqz.com>
2015-09-23 17:56:56 +0200

    DD-35 Currency Format Change

--- generic/static/views/currency.js
+++ generic/static/views/currency.js
@@ -2,18 +2,17 @@ function enhanceResults(data) {
   if (typeof Intl != "undefined" && Intl.NumberFormat) {
     data.CurrencyFormatSuport = true;
 
-    var cur_amount = data.toAmount.main.replace(/,/g, '.'),
     //First param is the Locale: en-US....
-      currency_formatter = new Intl.NumberFormat(CliqzUtils.PREFERRED_LANGUAGE, {
-        style: 'currency',
-        currency: data.toCurrency,
-        minimumFractionDigits: 2,
-      });
+    currency_formatter = new Intl.NumberFormat(CliqzUtils.PREFERRED_LANGUAGE, {
+      style: 'currency',
+      currency: data.toCurrency,
+      minimumFractionDigits: 2,
+    });
+
     if (data.toAmount.main) {
-      data.toAmount.main = currency_formatter.format(parseFloat(cur_amount));
+      data.toAmount.main = currency_formatter.format(data.toAmount.main);
     }
   } else {
     data.CurrencyFormatSuport = false;
   }
-
 }
