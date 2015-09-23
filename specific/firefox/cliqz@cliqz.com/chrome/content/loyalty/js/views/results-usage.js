1d8d398cc30d3d538681402a55308d4397cf7d8e
Thuy N Tran <thuy@cliqz.com>
2015-09-23 12:46:39 +0200

    indentation for templates

--- specific/firefox/cliqz@cliqz.com/chrome/content/loyalty/js/views/results-usage.js
+++ specific/firefox/cliqz@cliqz.com/chrome/content/loyalty/js/views/results-usage.js
@@ -63,33 +63,35 @@ window.SCRIPTS["results-usage"] = {
      */
     var usage = {
       data: [
-        {'data': [
-          {'val': stats_term.resultsCliqz.total, 'til': ["Mit", "CLIQZ"]},
-          {'val': stats_term.resultsGoogle, 'til': ["Mit anderen", "Suchmaschinen"]}
-        ],
-          'center_name': ['ABSOLUTE', 'SUCHANFRAGEN'],
-          'name': 'CLIQZ-Nutzung'
+        {
+          "data": [
+            {"val": stats_term.resultsCliqz.total, "til": ["Mit", "CLIQZ"]},
+            {"val": stats_term.resultsGoogle, "til": ["Mit anderen", "Suchmaschinen"]}
+          ],
+          "center_name": ["ABSOLUTE", "SUCHANFRAGEN"],
+          "name": "CLIQZ-Nutzung"
         },  // use, not use Cliqz result
-        {'data': [
-          {'val': stats_term.resultsCliqz.active, 'til': ["Mit", "Dropdown-Auswahl"]},
-          {'val': stats_term.resultsCliqz.auto, 'til': ["Mit", "Autocomplete"]}
-        ],
-          'center_name': ['CLIQZ', 'SUCHANFRAGEN'],
-          'name': 'CLIQZ-Vorschläge'
+        {
+          "data": [
+            {"val": stats_term.resultsCliqz.active, "til": ["Mit", "Dropdown-Auswahl"]},
+            {"val": stats_term.resultsCliqz.auto, "til": ["Mit", "Autocomplete"]}
+          ],
+          "center_name": ["CLIQZ", "SUCHANFRAGEN"],
+          "name": "CLIQZ-Vorschläge"
         },
-        {'data': [
-          {'val': stats_term.resultsCliqz.bigMachine, 'til': ["Mit", "einfachem Ergebnis"]},
-          {'val': stats_term.resultsCliqz.ez, 'til': ["Mit", "SmartCliqz"]},
-          {'val': stats_term.resultsCliqz.history, 'til': ["Mit", "Chronik"]}
-        ],
-          'center_name': ['CLIQZ', 'AUSWAHL'],
-          'name': 'CLIQZ-Ergebnisse'
+        {
+          "data": [
+            {"val": stats_term.resultsCliqz.bigMachine, "til": ["Mit", "einfachem Ergebnis"]},
+            {"val": stats_term.resultsCliqz.ez, "til": ["Mit", "SmartCliqz"]},
+            {"val": stats_term.resultsCliqz.history, "til": ["Mit", "Chronik"]}
+          ],
+          "center_name": ["CLIQZ", "AUSWAHL"],
+          "name": "CLIQZ-Ergebnisse"
         }
       ],
 
       width: 410, height: 310, R: 90, R2: 57,
       x0: null, y0: 80,
-//            g: 150,
       pie_CvsG: null, // pie_CvsG = Cliqz vs Gooogle
       svg: null
     };
@@ -110,84 +112,6 @@ window.SCRIPTS["results-usage"] = {
     svgs.append("svg:g")
       .attr("transform", function (d, i) {
         return "translate(" + usage.x0 + "," + (usage.y0 + usage.R) + ")";
-      })
-      .each(function (d) {
-        usage.pie_CvsG.drawPie(d3.select(this), d);
-      });
-  },
-
-  draw_usage_charts_v1: function (stats_term) {
-    // ------------- draw pie chart for user's data------------------//
-    /*
-     This is V1 (1st) of pie charts, with animation when hovering over the charts and subtitle appears
-     */
-    var usage = {
-      data: [
-        {'data': [
-          {'val': stats_term.resultsCliqz.total, 'til': "Cliqz Used",
-            'st': "In the past 30 days, you have made {0} searches. <br/>{1} of the time, you used CLIQZ ({2} times).<br/> <i>The 2.chart shows further details of used Cliqz.</i>",
-            'get_st': function () {
-              return String.format(this.st, ('' + this['extra']['total'] || '-'), Math.round(100 * this["val"] / this['extra']['total']) + '%', this.val + '');
-            }
-          },
-          {'val': stats_term.resultsGoogle, 'til': "Not Cliqz",
-            'st': "In the past 7 days, you have made {0} searches. <br/>You did not use CLIQZ for {1} results.",
-            'get_st': function () {
-              return String.format(this.st, ('' + this['extra']['total'] || ''), this.val + '');
-            }
-          }
-        ],
-          'name': 'CLIQZ USAGE'
-        },  // use, not use Cliqz result
-        {'data': [
-          {'val': stats_term.resultsCliqz.active, 'til': "Active",
-            'st': "Among {0} times you used CLIQZ, {1} times you used the <b>drop-down window results</b>. <br/> <i>The 3.chart shows details about result-types you used.</i>",
-            'get_st': function () {
-              return String.format(this.st, ('' + this['extra']['total'] || ''), this.val + '');
-            }},
-          {'val': stats_term.resultsCliqz.auto, 'til': "Autocomplete",
-            'st': "Among {0} times you used CLIQZ, {1} times you used <b>autocomplete</b>.",
-            'get_st': function () {
-              return String.format(this.st, ('' + this['extra']['total'] || ''), this.val + '');
-            }
-          }
-        ],
-          'name': 'INTERACTION TYPES'},
-        {'data': [
-          {'val': stats_term.resultsCliqz.bigMachine, 'til': "Basic Result",
-            'st': "Among {0} times you used CLIQZ results in the drop-down window, {1} times you used the <b>Basic Results</b>.",
-            'get_st': function () {
-              return String.format(this.st, ('' + this['extra']['total'] || ''), this.val + '');
-            }},
-          {'val': stats_term.resultsCliqz.ez, 'til': "SmartCliqz",
-            'st': "Among {0} times you used CLIQZ results in the drop-down window, {1} times you used <b>Smart Cliqz</b>.",
-            'get_st': function () {
-              return String.format(this.st, ('' + this['extra']['total'] || ''), this.val + '');
-            }},
-          {'val': stats_term.resultsCliqz.history, 'til': "History",
-            'st': "Among {0} times you used CLIQZ results in the drop-down window, {1} times you used <b>Cliqz History</b>.",
-            'get_st': function () {
-              return String.format(this.st, ('' + this['extra']['total'] || ''), this.val + '');
-            }}
-        ],
-          'name': 'RESULT TYPES'}
-      ],
-
-      width: 1050, height: 350, R: 90, R2: 57,
-      x0: 75, g: 150, y0: 70,
-      pie_CvsG: null, // pie_CvsG = Cliqz vs Gooogle
-      svg: null
-    };
-
-    usage.pie_CvsG = new CliqzUsePieChart(usage.R, usage.R2);  // pie_CvsG = Cliqz vs Gooogle
-
-    usage.svg = d3.select("#rootSVG");
-    usage.svg.selectAll("*").remove();
-    usage.svg.attr("width", usage.width).attr("height", usage.height)
-      .selectAll(".CLQ_USESAGE_gPie").data(usage.data).enter().append("svg:g")
-      .attr("class", "CLQ_USESAGE_Pie")
-      .attr("transform", function (d, i) {
-        return "translate(" + (usage.x0 + i * (usage.g + usage.R + usage.R) + usage.R) + "," + (usage.y0 + usage.R) + ")";
       })
       .each(function (d) {
         usage.pie_CvsG.drawPie(d3.select(this), d);
