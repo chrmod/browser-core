ea3751699170967840fa6bcb5c61bb6a4e0b7861
Andrey Mukomolov <andrey@cliqz.com>
2015-09-29 12:44:54 +0200

    Rewrite SmartCliqzScreenshot module to use new directory sctructure

---
+++ specific/firefox/cliqz@cliqz.com/tests/screenshots/active/SmartCliqzScreenshotTest.js
@@ -0,0 +1,20 @@
+TESTS.SmartCliqzTest = prepareScreenshotTest({
+    emails: ['andrey@cliqz.com'],
+    subject: 'Test screenshots report',
+    queries: {
+          'top':
+              ['google.de', 'g', 'f', 'y', 'goo', 'fa', 'www.google.de', 'face',
+               'go', 'web.de', 'you', 'gmx.de', 'ebay.de', 'google', 'bild.de', 'fac',
+               'ama', 'amazon.de', 'ebay', 'we'],
+          'smartcliqz':
+              ['flug LH76', '500 EUR in USD', '5m in inch',
+               'aktuelle uhrzeit los angeles', 'aktie apple',
+               'wetter in muenchen',
+               'spiegel.de', 'amazon.de', 'dkb.de'],
+          'thuy':
+              ['wetter m', 'wetter ber', 'bier',
+               'http://www.imdb.com/title/tt0499549', 'imdb ava',
+               'http://allrecipes.com/Recipe/Beef-Pho',
+               'http://www.imdb.com/title/tt0121766']
+    }
+});
