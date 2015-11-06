db588e9264e874c7e98b9a1721c70451e7b3cb0a
Lucian Corlaciu <lucian@cliqz.com>
2015-11-06 15:24:09 +0100

    dropdown width - move specific firefox fix outside of generic path

--- generic/modules/local/UI.js
+++ generic/modules/local/UI.js
@@ -135,13 +135,18 @@ var UI = {
         handlePopupHeight(box);
     },
     handleResults: function(){
+      // TODO: this is FF specific - move it to another place!
       var popup = urlbar.popup,
         data = [],
         ctrl = popup.mInput.controller,
         q = ctrl.searchString.replace(/^\s+/, '').replace(/\s+$/, ''),
-        lastRes = CliqzAutocomplete.lastResult;
+        lastRes = CliqzAutocomplete.lastResult,
+        width = Math.max(urlbar.clientWidth, 500);
 
-      //popup.style.height = "302px";
+      // set the width
+      // simply setting the width on the popup element and allowing the content to be 100% doenst work
+      gCliqzBox.style.width = width + 1 + "px"
+      gCliqzBox.resultsBox.style.width = width + (CliqzUtils.isWindows() ? -1 : 1) + "px"
 
       for(var i=0; i<popup._matchCount; i++) {
           data.push({
@@ -216,12 +221,6 @@ var UI = {
 
         //might be unset at the first open
         CLIQZ.Core.popup.mPopupOpen = true;
-
-        var width = Math.max(urlbar.clientWidth,500)
-
-        // set the width
-        gCliqzBox.style.width = width + 1 + "px"
-        gCliqzBox.resultsBox.style.width = width + (CliqzUtils.isWindows() ? -1 : 1) + "px"
 
         // try to find and hide misaligned elemets - eg - weather
         setTimeout(function(){
