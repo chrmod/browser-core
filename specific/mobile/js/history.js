7fc74da5c2ed2993f3acf157d0a948f90367a457
Khaled Tantawy <khaled@cliqz.com>
2015-12-23 11:08:32 +0100

    -- using conversations template -- adding a test function for ios to check the availability of the history web view when in background

--- specific/mobile/js/history.js
+++ specific/mobile/js/history.js
@@ -1,34 +1,55 @@
 'use strict';
 
 function showHistory(history) {
+  var data = [];
   history = history.results;
   var queries = [];
   var q = localStorage.getItem("recentQueries");
   if(q) {
     queries = JSON.parse(q);
   }
+
+  history.reverse();
+  queries.reverse();
   var hi = 0;
   var qi = 0;
   while(true) {
     if(hi >= history.length || qi >= queries.length) {
       break;
     }
-    if(history[hi].timestamp > queries[qi].timestamp) {
-      append(history[hi].title, history[hi].url, "link");
+    if(history[hi].timestamp <= queries[qi].timestamp) {
+//       append(history[hi].title, history[hi].url, "link");
+      data.push(history[hi]);
       hi++;
     } else {
-      append(queries[qi].query, ((Date.now() - queries[qi].timestamp) / 1000).toFixed(0) + " seconds ago", "query");
+//       append(queries[qi].query, ((Date.now() - queries[qi].timestamp) / 1000).toFixed(0) + " seconds ago", "queries");
+      data.push(queries[qi]);
       qi++;
     }
   }
   while(hi < history.length) {
-    append(history[hi].title, history[hi].url, "link");
+//     append(history[hi].title, history[hi].url, "link");
+    data.push(history[hi]);
     hi++;
   }
   while(qi < queries.length) {
-    append(queries[qi].query, ((Date.now() - queries[qi].timestamp) / 1000).toFixed(0) + " seconds ago", "query");
+    // append(queries[qi].query, ((Date.now() - queries[qi].timestamp) / 1000).toFixed(0) + " seconds ago", "queries");
+    data.push(queries[qi]);
     qi++;
   }
+  displayData(data);
+}
+
+function displayData(data) {
+  if(!CliqzHandlebars.tplCache["conversations"]) {
+    return setTimeout(displayData, 100, data);
+  }
+  document.body.innerHTML = CliqzHandlebars.tplCache["conversations"]({data: data});
+  document.body.scrollTop = 5000
+}
+
+function testActiveWebViewOnIos() {
+  document.body.innerHTML += "Test Succeeded";
 }
 
 function append(title, timestamp, styleClass) {
@@ -38,7 +59,7 @@ function append(title, timestamp, styleClass) {
   head.setAttribute('class', 'text-big');
   head.innerHTML = title;
   var p = document.createElement('p');
-  p.setAttribute('class', 'text-small');
+  p.setAttribute('class', 'time');
   p.innerHTML = timestamp;
   div.appendChild(head);
   div.appendChild(p);
@@ -47,4 +68,14 @@ function append(title, timestamp, styleClass) {
 
 window.addEventListener('load', function() {
   osBridge.searchHistory("", "showHistory")
+});
+
+Handlebars.registerHelper('conversationsTime', function(time) {
+    var d = new Date(time);
+    var hours = d.getHours();
+    hours = hours > 9 ? hours : '0' + hours
+    var minutes = d.getMinutes();
+    minutes = minutes > 9 ? minutes : '0' + minutes
+    var formatedDate = hours + ':' + minutes;
+    return formatedDate;
 });
