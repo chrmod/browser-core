02d74fdcbad39300861cb4884e8beaf04440e7ef
Stefan Schraufstetter <stefan@cliqz.com>
2016-02-17 15:44:48 +0100

    EX-1890 Sublinks don't work on iOS8

--- specific/mobile/js/debug.js
+++ specific/mobile/js/debug.js
@@ -55,6 +55,11 @@ Handlebars.registerHelper('ifShowSearch', function(results, options) { // if equ
 });
 
 
+Handlebars.registerHelper('mobileWikipediaUrls', function(url) { 
+  return url.replace("http://de.wikipedia.org/wiki","https://de.m.wikipedia.org/wiki");
+});
+
+
 
 function trace() {
   try {
