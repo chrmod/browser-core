00560902589bd849c5821f7c645d4e2206f29674
Stefan Schraufstetter <stefan@cliqz.com>
2016-02-26 11:59:53 +0100

    EX-2039 Mobile - very small bug fixes

--- generic/modules/local/views/_generic.js
+++
@@ -1,9 +0,0 @@
-export default {
-  enhanceResults(data) {
-debugger;
-      if(data.news) {
-          
-      }
-    //CliqzUtils.getLogoDetails(CliqzUtils.getDetailsFromUrl(urls[i].url))
-  }
-};
