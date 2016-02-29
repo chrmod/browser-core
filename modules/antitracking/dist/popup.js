32e1846a3a3d786c60db71d6b66e7e5f94eed9fc
Gabriel <gabriel@cliqz.com>
2016-02-29 11:31:42 +0100

    CB-1751 No Bad tracker +No trackers msgs state add

--- modules/antitracking/dist/popup.js
+++ modules/antitracking/dist/popup.js
@@ -6,7 +6,7 @@ var attPopUp = document.querySelector(".cqz-antitracking-popup"),
     hostname;
 
 function setBodyClass(options) {
-  if( options.enabled ) {
+  if (options.enabled) {
     document.body.classList.add("cqz-attrack-enabled");
     document.body.classList.remove("cqz-attrack-disabled");
   } else {
@@ -14,36 +14,36 @@ function setBodyClass(options) {
     document.body.classList.remove("cqz-attrack-enabled");
   }
 
-  if( options.whitelisted ) {
+  if (options.whitelisted) {
     document.body.classList.add("cqz-domain-in-whitelist");
   } else {
     document.body.classList.remove("cqz-domain-in-whitelist");
   }
 
   // If ATT enabled and there is no site
-  if( !options.url && options.enabled ) {
-     document.body.classList.add("cqz-no-site");
+  if (!options.url && options.enabled) {
+    document.body.classList.add("cqz-no-site");
   } else {
     document.body.classList.remove("cqz-no-site");
   }
 
   //If thare are ANY tracker
-  if( !options.have_trackers ) {
-     document.body.classList.add("cqz-have-no-trackers");
+  if (!options.have_trackers) {
+    document.body.classList.add("cqz-have-no-trackers");
   } else {
     document.body.classList.remove("cqz-have-no-trackers");
   }
 
   //If thare are BAD tracker
-  if( options.have_bad_trackers ) {
-     document.body.classList.add("cqz-have-bad-trackers");
+  if (options.have_bad_trackers) {
+    document.body.classList.add("cqz-have-bad-trackers");
   } else {
     document.body.classList.remove("cqz-have-bad-trackers");
   }
 
   //If thare are NO BAD tracker
-  if( options.have_trackers && !options.have_bad_trackers ) {
-     document.body.classList.add("cqz-have-no-bad-trackers");
+  if (options.have_trackers && !options.have_bad_trackers) {
+    document.body.classList.add("cqz-have-no-bad-trackers");
   } else {
     document.body.classList.remove("cqz-have-no-bad-trackers");
   }
@@ -72,7 +72,7 @@ function populateDOM() {
     var general_msg_trnsl = document.querySelector(".cqz-general-trackers-msg");
     var general_trackers_count = data.cookiesCount + data.requestsCount;
     var have_bad_trackers_bool = general_trackers_count > 0;
-    var have_any_trackers_bool = Object.keys(data.trakersList.trackers).length;
+    var have_any_trackers_bool = Object.keys(data.trakersList.trackers).length > 0;
 
     ////Display Trackers list
     var counterTrackers = 0;
