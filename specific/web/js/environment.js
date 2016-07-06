ebc2e0f67c3d05fd241db9a9a6336959c2cd1f94
Lucian Corlaciu <lucian@cliqz.com>
2016-07-06 09:49:09 +0200

    Revert "EX-2538: Don't send result telemetry from private tabs"

--- specific/web/js/environment.js
+++ specific/web/js/environment.js
@@ -28,7 +28,6 @@ CLIQZEnvironment = {
     },
     OS: 'darwin',
     isPrivate: function(){ return false; },
-    getCurrentTabPrivateness: function(window, callback) { return false; },
     getWindow: function(){ return window; },
     httpHandler: function(method, url, callback, onerror, timeout, data){
         var req = new XMLHttpRequest();
