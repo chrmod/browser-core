aacf65c5f6eed158d8401527578b728c65a69de2
Lucian Corlaciu <lucian@cliqz.com>
2017-02-15 10:14:35 +0100

    V2.13.0 feedback (#3030)
    * Remove lint-tests for production builds
    
    * Move remote images required by SimpleUI inside the extension

--- broccoli/modules-tree.js
+++ broccoli/modules-tree.js
@@ -360,7 +360,8 @@ const bowerTree = new MergeTrees([
   new Funnel(bowerComponents, { include: Array.from(requiredBowerComponents) }),
 ]);
 
-const styleCheckTestsTree = getLintTestsTree();
+const styleCheckTestsTree = cliqzConfig.environment === 'production' ?
+  new MergeTrees([]) : getLintTestsTree();
 
 
 module.exports = {
